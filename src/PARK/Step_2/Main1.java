f3f2624f6689b09cb1a2b3db57305250376d4a8a
Parkseojin <seojin5565@gmail.com>
2024-02-05 13:26:22 +0900

    [add] : 2-1번 문제품

---
+++ src/PARK/Step_2/Main1.java
@@ -0,0 +1,25 @@
+package PARK.Step_2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main1 {
+
+  public static void main(String[] args) throws Exception {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    int n1 = Integer.parseInt(st.nextToken());
+    int n2 = Integer.parseInt(st.nextToken());
+
+    if(n1 > n2){
+      System.out.println(">");
+    }else if (n1 < n2){
+      System.out.println("<");
+    }else {
+      System.out.println("==");
+    }
+  }
+}
