7929b74ec584adb96abd916afe0fea9f32897c13
Dayun Lee <wohd7877@naver.com>
2024-02-07 22:31:35 +0900

    [add] 2-3까지 문풀

---
+++ src/LEE/Step_2/Main3.java
@@ -0,0 +1,18 @@
+package LEE.Step_2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main3 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int year = Integer.parseInt(br.readLine());
+
+        if(year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
+            System.out.println(1);
+        }else {
+            System.out.println(0);
+        }
+    }
+}
